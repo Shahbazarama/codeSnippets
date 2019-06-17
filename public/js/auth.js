fa442b3923a6a4c48d6433c171283b4be8447594
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-06-17 16:35:39 -0700

    base authentication through firebase

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,30 +1,54 @@
-const signupForm = document.querySelector('#signupForm');
-const loginForm = document.querySelector('#loginForm');
+(function() {
 
-signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
 
-  const email = document.querySelector('#signupEmail').value;
-  const password = document.querySelector('#signupPassword').value;
+  const signupForm = document.querySelector('#signupForm');
+  const loginForm = document.querySelector('#loginForm');
+  const logoutButton = document.querySelector('#logoutButton');
+  const userWelcomeText = document.querySelector('#userWelcomeText');
 
-  // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then( (user) => {
-    $('#signupModal').modal('toggle');
-    signupForm.reset();
-    console.log(user)
+  // listen for auth status changes
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      userWelcomeText.innerHTML = 'Welcome, ' + user.email
+    } else {
+      userWelcomeText.innerHTML = ''
+    }
+  })
+
+  // sign up
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.querySelector('#signupEmail').value;
+    const password = document.querySelector('#signupPassword').value;
+
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then((user) => {
+      $('#signupModal').modal('toggle');
+      signupForm.reset();
+      console.log(user)
+    });
+  })
+
+  // login
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.querySelector('#loginEmail').value;
+    const password = document.querySelector('#loginPassword').value;
+
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((user) => {
+      $('#loginModal').modal('toggle');
+      loginForm.reset();
+      console.log(user)
+    });
   });
-})
 
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const email = document.querySelector('#loginEmail').value;
-  const password = document.querySelector('#loginPassword').value;
-
-  // log the user in
-  auth.signInWithEmailAndPassword(email, password).then( (user) => {
-    $('#loginModal').modal('toggle');
-    loginForm.reset();
-    console.log(user)
+  // logout
+  logoutButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
   });
-});
+
+}());
